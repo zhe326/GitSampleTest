21cb84c8a3eeedd4e064013314041acfb7d228db
yy870326 <yy870326@gmail.com>
2022-08-22 19:39:03 +0800

    math

--- Gitproject/src/testApp/Main.java
+++ Gitproject/src/testApp/Main.java
@@ -6,6 +6,15 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		System.out.println("hello world2");
+		//這是新增數學
+		Math math = new Math();
+		System.out.println(math.add(2, 1));
 	}
 
+}
+
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
